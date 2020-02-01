5a10c22508ec4467a8c9060e0efb6a9962062ad8
Khoa Mai <kmai@alpha-sense.com>
2020-02-02 01:24:34 +0200

    parallel read proc status

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -21,20 +22,42 @@ func main() {
 			return nil, fmt.Errorf("Failed to list processes %v", err)
 		}
 
+		var wg sync.WaitGroup
 		var out []proc.ProcStatus
+		errChan := make(chan error)
+		done := make(chan interface{})
+
 		for _, p := range procList {
-			ps, err := proc.NewStatus(p.PID, fmt.Sprintf("/proc/%d/status", p.PID))
-			if err != nil {
-				return nil, fmt.Errorf("Failed to read /proc/%d/status %v", p.PID, err)
-			}
-			out = append(out, ps)
+			wg.Add(1)
+
+			// Parallel read status
+			go func(pid int) {
+				defer wg.Done()
+
+				ps, err := proc.NewStatus(pid, fmt.Sprintf("/proc/%d/status", pid))
+				if err != nil {
+					errChan <- err
+				}
+				out = append(out, ps)
+			}(p.PID)
 		}
+
+		go func() {
+			wg.Wait()
+			close(done)
+		}()
+
+		select {
+		case <-done:
+		case err := <-errChan:
+			return nil, fmt.Errorf("Failed to read status %v", err)
+		}
+
 		return out, nil
 	}
 
 	c := newCollector(stats)
 	prometheus.MustRegister(c)
-
 	http.Handle("/metrics", promhttp.Handler())
 	logger.Info("Starting exporter at :8080/metrics")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
--- proc/proc.go
+++ proc/proc.go
@@ -10,22 +10,20 @@ type Proc struct {
 	PID int
 }
 
-type Procs []Proc
-
 // AllProcs returns a list of all currently available processes.
-func AllProcs() (Procs, error) {
+func AllProcs() ([]Proc, error) {
 	d, err := os.Open("/proc")
 	if err != nil {
-		return Procs{}, err
+		return []Proc{}, err
 	}
 	defer d.Close()
 
 	names, err := d.Readdirnames(-1)
 	if err != nil {
-		return Procs{}, fmt.Errorf("could not read %s: %s", d.Name(), err)
+		return []Proc{}, fmt.Errorf("could not read %s: %s", d.Name(), err)
 	}
 
-	p := Procs{}
+	p := []Proc{}
 	for _, n := range names {
 		pid, err := strconv.ParseInt(n, 10, 64)
 		if err != nil {
